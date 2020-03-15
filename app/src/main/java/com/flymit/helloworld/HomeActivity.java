dcc90294ac8d2c6544107d9bc02d8357242b00a8
AmitKadam <amit.bk03@gmail.com>
2020-03-15 22:54:57 +0530

    Text Splash Screen added

---
+++ app/src/main/java/com/flymit/helloworld/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.flymit.helloworld;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HomeActivity extends AppCompatActivity {
+    private WebView view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //String myurl = "http://amazon.in";
+        String myurl = "file:///android_asset/index.html";
+        view = (WebView) this.findViewById(R.id.webView);
+
+        view.setWebChromeClient(new WebChromeClient());
+        view.setWebViewClient(new WebViewClient());
+        view.getSettings().setDomStorageEnabled(true);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(myurl);
+    }
+
+    public void onBackPressed()
+    {
+        if(view.canGoBack()){
+            view.goBack();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Exit!")
+                    .setMessage("Are you sure you want to close?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
+    }
+}
--- app/src/main/java/com/flymit/helloworld/MainActivity.java
+++ app/src/main/java/com/flymit/helloworld/MainActivity.java
@@ -4,47 +4,28 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView view;
+
+    private static int SPLASH_TIMEOUT=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //String myurl = "http://amazon.in";
-        String myurl = "file:///android_asset/index.html";
-        view = (WebView) this.findViewById(R.id.webView);
 
-        view.setWebChromeClient(new WebChromeClient());
-        view.setWebViewClient(new WebViewClient());
-        view.getSettings().setDomStorageEnabled(true);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(myurl);
-    }
-
-    public void onBackPressed()
-    {
-        if(view.canGoBack()){
-            view.goBack();
-        }else{
-            new AlertDialog.Builder(this)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle("Exit!")
-                    .setMessage("Are you sure you want to close?")
-                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
-                    {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-
-                    })
-                    .setNegativeButton("No", null)
-                    .show();
-        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
