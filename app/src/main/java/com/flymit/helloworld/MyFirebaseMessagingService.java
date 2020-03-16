faa71e0a0fb6dd47c319a12ca9e6cb5ab8efda79
AmitKadam <amit.bk03@gmail.com>
2020-03-17 00:16:21 +0530

    Notifications implemented with app

---
+++ app/src/main/java/com/flymit/helloworld/MyFirebaseMessagingService.java
@@ -0,0 +1,36 @@
+package com.flymit.helloworld;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+            super.onMessageReceived(remoteMessage);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                    .setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setStyle(new NotificationCompat.BigTextStyle())
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, notificationBuilder.build());
+    }
+}
